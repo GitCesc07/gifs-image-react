76926a63d86782867712a5caaaf60f8a9521bfa3
CescDevGit07 <cesar04071997@gmail.com>
2023-06-01 19:02:56 -0600

    Consumo de API

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,20 @@
+
+const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=30&api_key=b006lFgItsRvqKjF0JnpCkMtm5k6Oqbo`;
+
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+
+  const gifs = data.map(img => {
+    return {
+      id: img.id,
+      title: img.title,
+      url: img.images?.downsized_medium.url
+    }
+  })
+
+  return gifs;
+}
+
+export default getGifs;
